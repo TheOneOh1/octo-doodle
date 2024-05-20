9f30cd4eb7fc6413977a98bc6f94d56a7aeabd0f
Anand Kore <103954920+TheOneOh1@users.noreply.github.com>
2024-05-20 06:26:16 +0000

    added container kill script

---
+++ kill_container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if the container is running
+container_status=$(sudo docker ps -q -f name=octo-doodle)
+
+if [ -n "$container_status" ]; then
+  echo "Container is running. Stopping and removing the container..."
+  sudo docker stop octo-doodle
+  sudo docker rm octo-doodle
+  echo "Container stopped and removed."
+else
+  echo "Container already killed."
+fi
