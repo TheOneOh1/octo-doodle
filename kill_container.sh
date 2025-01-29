a29c50b11f7c00688b714d12cd6d07e1dbebcb01
Anand Kore <103954920+TheOneOh1@users.noreply.github.com>
2025-01-29 04:22:43 +0000

    deploy

--- kill_container.sh
+++ kill_container.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 # Check if the container is running
-container_status=$(sudo docker ps -a -q -f name=$1)  # Command injection vulnerability
-
-# Dangerous: Creating world-writable log file
-touch /var/log/container.log
-chmod 777 /var/log/container.log
+container_status=$(sudo docker ps -a -q -f name=octo-doodle)
 
 if [ -n "$container_status" ]; then
   echo "Container is running. Stopping and removing the container..."
-  # Dangerous: Using eval with user input
-  eval "sudo docker stop $1"
-  eval "sudo docker rm $1"
-  # Dangerous: Logging with injection possibility
-  echo "Container $1 stopped at $(date)" >> /var/log/container.log
+  sudo docker stop octo-doodle
+  sudo docker rm octo-doodle
   echo "Container stopped and removed."
 else
   echo "Container already killed."
 fi
-
-# Dangerous: Exposing sensitive information
-echo "Docker daemon info:" >> /var/log/container.log
-sudo docker info >> /var/log/container.log
